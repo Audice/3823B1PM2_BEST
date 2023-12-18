b1f4b07c25617d8c79e5a602e93540b5e375ed00
Karchkov Denis <karchkov.denis@mail.ru>
2023-12-18 17:18:25 +0300

    Correct

--- 9-files/smyle_file/test.c
+++ 9-files/smyle_file/test.c
@@ -10,28 +10,33 @@ int correct_smile(char filename[]){
 void test_write_smile() {
     struct stat st; 
     char* filename = "base1.txt";
-    stat(filename, &st);
+    
     write_smile("base1.txt", "smile, uh...oh, ouch");
+    stat(filename, &st);
     TEST_ASSERT(st.st_size == 15);
 
     filename = "base2.txt";
-    stat(filename, &st);
+    
     write_smile("base2.txt", "happy, happier, sad, bored");
+    stat(filename, &st);
     TEST_ASSERT(st.st_size == 22);
 
     filename = "base3.txt";
     stat(filename, &st);
     write_smile("base3.txt", "serious, wink");
+    stat(filename, &st);
     TEST_ASSERT(st.st_size == 10);
 
     filename = "base4.txt";
-    stat(filename, &st);
+    
     write_smile("base4.txt", "i messed it up");
+    stat(filename, &st);
     TEST_ASSERT(st.st_size == 6);
 
     filename = "base5.txt";
-    stat(filename, &st);
+    
     write_smile("base5.txt", "jealous, happier, i messed it up, i love it");
+    stat(filename, &st);
     TEST_ASSERT(st.st_size == 25);
 }
 
