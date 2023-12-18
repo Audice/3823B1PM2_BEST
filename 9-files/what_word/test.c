0945e73612b84b55d381636d3133b1772668a0c6
Karchkov Denis <karchkov.denis@mail.ru>
2023-12-18 13:05:33 +0300

    Correct

--- 9-files/what_word/test.c
+++ 9-files/what_word/test.c
@@ -4,8 +4,8 @@
 
 void test_word_finder() {
     TEST_ASSERT(strcmp(get_word("../../TestingFiles/Task4_1.txt", 13), "that") == 0);
-    TEST_ASSERT(strcmp(get_word("../../TestingFiles/Task4_2.xml", 12), "game") == 0);
-    TEST_ASSERT(strcmp(get_word("../../TestingFiles/Task4_3.xml", 11), "young") == 0);
+    TEST_ASSERT(strcmp(get_word("../../TestingFiles/Task4_2.txt", 12), "game") == 0);
+    TEST_ASSERT(strcmp(get_word("../../TestingFiles/Task4_3.txt", 11), "young") == 0);
 }
 
 
